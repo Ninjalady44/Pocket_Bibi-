ae58b41f5387f8a5a6807b74ee4265bc98843cdd
Richard Als <richard.als@hotmail.com>
2026-02-04 20:17:26 -0500

    Adding Egg Selection

---
+++ PocketBibi/Assets/Data/BibiData.cs
@@ -0,0 +1,21 @@
+/****************************************************************************************
+ * Copyright: Ninja Lady Game Development
+ * Script: BibiData.cs
+ * Date Created: February 4 2026
+ * Created By: Richard Als
+ * Description: ScriptableObject to hold default game information.
+ ****************************************************************************************
+ * Modified By: Richard Als
+ * Date Last Modified: February 4 2026
+ ****************************************************************************************
+ * TODO: 
+ * Known Bugs:
+ ****************************************************************************************/
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BibiData", menuName = "ScriptableObjects/BibiData")]
+public class BibiData : ScriptableObject
+{
+    
+}
--- PocketBibi/Assets/Scripts/MainMenu.cs
+++ PocketBibi/Assets/Scripts/MainMenu.cs
@@ -6,12 +6,13 @@
  * Description: Handles menu buttons and panels
  ****************************************************************************************
  * Modified By: Richard Als
- * Date Last Modified: January 30 2026
+ * Date Last Modified: February 4 2026
  ****************************************************************************************
  * TODO: 
  * Known Bugs:
  ****************************************************************************************/
 
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PocketBibi
@@ -20,9 +21,48 @@ namespace PocketBibi
     {
         #region Private Variables/Fields Exposed to Inspector for Editing
 
+        [SerializeField] private GameObject _startButton = null;
+        [SerializeField] private GameObject _gameSelection = null;
+        [SerializeField] private GameObject _eggSelection = null;
+        [SerializeField] private GameObject _nameSelection = null;
+
         #endregion
 
         #region Private Variables/Fields used in this Class Only
+
+        private List<GameObject> _menuPanels;
+
+        #endregion
+
+        #region Private Initialization Functions/Methods
+
+        void Start()
+        {
+            _menuPanels = new List<GameObject>();
+            _menuPanels.Add(_startButton);
+            _menuPanels[0].SetActive(true);
+            _gameSelection.SetActive(false);
+            _eggSelection.SetActive(false);
+            _nameSelection.SetActive(false);
+        }
+
+        #endregion
+
+        #region Private Functions/Methods
+
+        private void NextMenuPanel(GameObject menu)
+        {
+            _menuPanels[0].SetActive(false);
+            _menuPanels.Insert(0, menu);
+            _menuPanels[0].SetActive(true);
+        }
+
+        private void PreviousMenuPanel()
+        {
+            _menuPanels[0].SetActive(false);
+            _menuPanels.RemoveAt(0);
+            _menuPanels[0].SetActive(true);
+        }
 
         #endregion
 
@@ -30,7 +70,27 @@ namespace PocketBibi
 
         public void StartGame()
         {
+            NextMenuPanel(_gameSelection);
+        }
 
+        public void NewGame()
+        {
+            NextMenuPanel(_eggSelection);
+        }
+
+        public void BackButton()
+        {
+            PreviousMenuPanel();
+        }
+
+        public void ExitButton()
+        {
+            Application.Quit();
+        }
+
+        public void EggButton(int egg)
+        {
+            NextMenuPanel(_nameSelection);
         }
 
         #endregion
