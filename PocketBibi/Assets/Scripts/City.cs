27eb411ca78d3212309e96d53231fe1c8472ce40
Richard Als <richard.als@hotmail.com>
2026-07-31 10:55:17 -0400

    Adding City script

---
+++ PocketBibi/Assets/Scripts/City.cs
@@ -0,0 +1,32 @@
+/****************************************************************************************
+ * Copyright: Ninja Lady Game Development
+ * Script: City.cs
+ * Date Created: July 31 2026
+ * Created By: Richard Als
+ * Description: 
+ ****************************************************************************************
+ * Modified By: Richard Als
+ * Date Last Modified: July 31 2026
+ ****************************************************************************************
+ * TODO: 
+ * Known Bugs:
+ ****************************************************************************************/
+
+using UnityEngine;
+
+namespace PocketBibi
+{
+    public class City : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _buildingLocations;
+
+        public void _arcadeButton() { }
+        public void _fishButton() { }
+        public void _hotelButton() { }
+        public void _infoButton() { }
+        public void _restaurantButton() { }
+        public void _schoolButton() { }
+        public void _veggiesButton() { }
+        public void _workButton() { }
+    }
+}
